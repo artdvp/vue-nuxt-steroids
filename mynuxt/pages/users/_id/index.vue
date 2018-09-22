f1447208e7b56b20c7ac2576553912bc2cb76b31
bonbonpa <sphinspf@gmail.com>
2018-09-22 23:20:23 +0700

    mynuxt-app : Validating Parameters

--- mynuxt/pages/users/_id/index.vue
+++ mynuxt/pages/users/_id/index.vue
@@ -3,3 +3,13 @@
         <h1>This is single users ID : {{ $route.params.id }}</h1>
     </div>
 </template>
+
+<script>
+export default {
+  validate(data) {
+      // console.log(data);
+      // return data.params.id == 1;
+      return /^\d+$/.test(data.params.id); // validate number only
+  }
+};
+</script>
