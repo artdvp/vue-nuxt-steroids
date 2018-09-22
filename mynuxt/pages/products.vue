d006a1349eb9b940fc00385cf8172227ca2f4115
bonbonpa <sphinspf@gmail.com>
2018-09-22 23:34:40 +0700

    mynuxt-app : Creating Nested Routes - Render Child Component

---
+++ mynuxt/pages/products.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1 class="title">This is Products page</h1>
+        <input type="text" v-model="pid">
+        <button @click="onLoadProduct">Load Products</button>
+        <nuxt-child />
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pid: ""
+    };
+  },
+  methods: {
+    onLoadProduct() {
+      this.$router.push("/products/" + this.pid);
+    }
+  }
+};
+</script>
--- mynuxt/pages/products/index.vue
+++ mynuxt/pages/products/index.vue
@@ -1,9 +1,3 @@
 <template>
-    <div>
-        <h1 class="title">
-            Products
-        </h1>
-        <nuxt-link to="/products/1">pId : 1</nuxt-link><br>
-        <nuxt-link to="/products/2">pId : 2</nuxt-link>
-    </div>
+    <p>Please enter an id and click the button </p>
 </template>
