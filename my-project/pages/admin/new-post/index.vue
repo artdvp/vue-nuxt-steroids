a7cbb228de3d3fa6b3b8c5589b107db29aca2840
bonbonpa <sphinspf@gmail.com>
2018-09-24 07:08:57 +0700

    mynuxt-app-blog : Creating an Admin Section1

---
+++ my-project/pages/admin/new-post/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="admin-new-post-page">
+        <section class="new-post-form">
+            <form action="">
+                
+            </form>
+        </section>
+    </div>
+</template>
