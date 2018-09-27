4bdb5a3100c9a835350d2a03b59cb24291533700
bonbonpa <sphinspf@gmail.com>
2018-09-28 00:19:50 +0700

    mynuxt-app-blog : Adding a Separate Layout to the Admin Area

--- my-project/components/Posts/PostList.vue
+++ my-project/components/Posts/PostList.vue
@@ -2,9 +2,9 @@
     <section class="post-list">
       <post-preview id="1" :is-admin="isAdmin" thumbnail="https://www.articlesplanet.info/wp-content/uploads/2018/05/Tech-sector.jpg" title="Hello there!"
         previewText="This my first post!"></post-preview>
-      <post-preview id="2" thumbnail="https://www.articlesplanet.info/wp-content/uploads/2018/05/Tech-sector.jpg" title="Hello !"
+      <post-preview id="2" :is-admin="isAdmin" thumbnail="https://www.articlesplanet.info/wp-content/uploads/2018/05/Tech-sector.jpg" title="Hello !"
         previewText="This my second post!"></post-preview>
-      <post-preview id="3" thumbnail="https://www.articlesplanet.info/wp-content/uploads/2018/05/Tech-sector.jpg" title="Hi!"
+      <post-preview id="3" :is-admin="isAdmin" thumbnail="https://www.articlesplanet.info/wp-content/uploads/2018/05/Tech-sector.jpg" title="Hi!"
         previewText="This my third post!"></post-preview>
     </section>
 </template>
