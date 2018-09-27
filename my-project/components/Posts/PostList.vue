5d38d618e87fa61072200c6bfe86a9ff971417b8
bonbonpa <sphinspf@gmail.com>
2018-09-28 00:03:11 +0700

    mynuxt-app-blog : Preparing the Backend

--- my-project/components/Posts/PostList.vue
+++ my-project/components/Posts/PostList.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="post-list">
-      <post-preview id="1" thumbnail="https://www.articlesplanet.info/wp-content/uploads/2018/05/Tech-sector.jpg" title="Hello there!"
+      <post-preview id="1" :is-admin="isAdmin" thumbnail="https://www.articlesplanet.info/wp-content/uploads/2018/05/Tech-sector.jpg" title="Hello there!"
         previewText="This my first post!"></post-preview>
       <post-preview id="2" thumbnail="https://www.articlesplanet.info/wp-content/uploads/2018/05/Tech-sector.jpg" title="Hello !"
         previewText="This my second post!"></post-preview>
@@ -15,6 +15,12 @@ import PostPreview from '@/components/Posts/PostPreview.vue'
 export default {
   components: {
     PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false  
+    }
   }
 }
 </script>
