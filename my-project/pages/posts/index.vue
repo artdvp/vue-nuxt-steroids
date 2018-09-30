0bf41d237568e9f4abfcf0e2a8bf8cb8e8a76e78
bonbonpa <sphinspf@gmail.com>
2018-09-30 23:38:25 +0700

    mynuxt-app-blog : Time to Practice - Server-Side Data

--- my-project/pages/posts/index.vue
+++ my-project/pages/posts/index.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="posts-page">
-    <post-list></post-list>
+    <post-list :posts="loadedPosts"></post-list>
   </div>
 </template>
 
 <script>
-import PostList from '@/components/Posts/PostList.vue'
+import PostList from "@/components/Posts/PostList.vue";
 
 export default {
   components: {
     PostList
+  },
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          {
+            id: "1",
+            title: "First Post",
+            previewText: "This is our first post!",
+            thumbnail:
+              "https://www.articlesplanet.info/wp-content/uploads/2018/05/Tech-sector.jpg"
+          },
+          {
+            id: "2",
+            title: "Second Post",
+            previewText: "This is our second post!",
+            thumbnail:
+              "https://www.articlesplanet.info/wp-content/uploads/2018/05/Tech-sector.jpg"
+          }
+        ]
+      });
+    }, 1500);
   }
-}
+};
 </script>
 
 <style scoped>
